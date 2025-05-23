d81e7241acc987f022556a4174f15d5251732d78
Brian Miller <Bmiller995@gmail.com>
2025-05-23 10:46:54 -0500

    intial commit with start of OrderDataManager class

---
+++ src/main/java/com/LeafandLoaf/data/OrderDataManager.java
@@ -0,0 +1,18 @@
+package com.LeafandLoaf.data;
+
+// This class manages the flow of ordering
+public class OrderDataManager {
+
+    // Holds the current order the user is making
+    private Order currentOrder;
+
+    // Starts the order flow
+    public void startOrder() {
+        System.out.println("Welcome to Leaf and Loaf");
+        // TODO: Add menu logic
+    }
+
+    // Method to add a sandwich
+    public
+
+}
---
+++ src/main/java/com/LeafandLoaf/ui/LeafandLoafApp.java
@@ -0,0 +1,10 @@
+package com.LeafandLoaf.ui;
+
+public class LeafandLoafApp {
+    public static void main(String[] args) {
+
+        OrderDataManager manager = new OrderDataManager();
+        manager.startOrder();
+
+    }
+}
